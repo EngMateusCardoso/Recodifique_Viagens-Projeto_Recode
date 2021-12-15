9c08cb87a9017616094214437fdef99022b2da97
Mateus Cardoso Oliveira <eng.mateuscardosooliveira@gmail.com>
2021-12-15 00:43:12 -0300

    Com js. Modelo minimo e suficiente para iniciar o ASP.NET MVC

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function exibir_estes(tipo){
+    let Destinos = document.getElementsByClassName('card');
+    if ( tipo == 'Todos'){
+        for (var i = 0; i < Destinos.length; i++){
+            Destinos[i].style = "display: block; width: 175px;"
+        }
+    }else{
+        for (var i = 0; i < Destinos.length; i++){
+            if ( tipo == Destinos[i].id)
+                Destinos[i].style = "display: block; width: 175px;"
+            else
+                Destinos[i].style = "display: none; width: 175px;"
+        }
+    }
+}
